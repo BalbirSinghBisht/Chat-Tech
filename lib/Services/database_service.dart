0f75394408509dc6f356aa3e85caa287ed55a3f5
balbir singh bisht <balbirbisht560@gmail.com>
2021-09-22 10:40:29 +0530

    initial commit

--- lib/Services/database_service.dart
+++ lib/Services/database_service.dart
@@ -38,7 +38,7 @@ class DatabaseService{
     });
   }
 
-  Future togglingGroupJoin(String groupId, String groupName, String userName) async{
+  Future togglingGroupJoin(String groupId, String groupName, String userName) async {
     DocumentReference userDocRef = userCollection.doc(uid);
     DocumentSnapshot userDocSnapshot = await userDocRef.get();
 
@@ -46,58 +46,58 @@ class DatabaseService{
 
     List<dynamic> groups = await userDocSnapshot.get('groups');
 
-    if(groups.contains(groupId+ '_' +groupName)){
+    if (groups.contains(groupId + '_' + groupName)) {
       await userDocRef.update({
-        'groups': FieldValue.arrayRemove([groupId+ '_' +groupName])
+        'groups': FieldValue.arrayRemove([groupId + '_' + groupName])
       });
       await groupDocRef.update({
-        'members': FieldValue.arrayRemove([uid+ '_' +userName])
+        'members': FieldValue.arrayRemove([uid + '_' + userName])
       });
+    }
+    else {
+      await userDocRef.update({
+        'groups': FieldValue.arrayUnion([groupId + '_' + groupName])
+      });
+      await groupDocRef.update({
+        'members': FieldValue.arrayUnion([uid + '_' + userName])
+      });
+    }
+  }
+  Future<bool> isUserJoined(String groupId, String groupName, String userName) async{
+    DocumentReference userDocRef =await userCollection.doc(uid);
+    DocumentSnapshot userDocSnapshot = await userDocRef.get();
+
+    List<dynamic> groups = await userDocSnapshot.get('groups');
+
+    if(groups.contains(groupId+ '_' +groupName)){
+      return true;
     }
     else{
-      await userDocRef.update({
-        'groups': FieldValue.arrayUnion([groupId+ '_' +groupName])
-      });
-      await groupDocRef.update({
-        'members': FieldValue.arrayUnion([uid+ '_' +userName])
-      });
+      return false;
     }
-    Future<bool> isUserJoined(String groupId, String groupName, String userName) async{
-      DocumentReference userDocRef =await userCollection.doc(uid);
-      DocumentSnapshot userDocSnapshot = await userDocRef.get();
-
-      List<dynamic> groups = await userDocSnapshot.get('groups');
-
-      if(groups.contains(groupId+ '_' +groupName)){
-        return true;
-      }
-      else{
-        return false;
-      }
-    }
-    Future getUserData(String email) async{
-      QuerySnapshot snapshot = await userCollection.where('email', isEqualTo: email).get();
-      print(snapshot.docs[0].data());
-      return snapshot;
-    }
-    getUserGroups() async{
-      return FirebaseFirestore.instance.collection('users').doc(uid).snapshots();
-    }
-    sendMsg(String groupId, chatMsgData){
-      FirebaseFirestore.instance.collection('groups').doc(groupId).collection('messages').add(chatMsgData);
-      FirebaseFirestore.instance.collection('groups').doc(groupId).update({
-        'recentMessage': chatMsgData['message'],
-        'recentMessageSender': chatMsgData['sender'],
-        'recentMessageTime': chatMsgData['time'].toString(),
-      });
-    }
-    getChats(String groupId) async{
-      return FirebaseFirestore.instance.collection('groups').doc(groupId)
-          .collection('messages').orderBy('time').snapshots();
-    }
-    searchByName(String groupName){
-      return FirebaseFirestore.instance.collection('groups').
-      where('groupName', isEqualTo: groupName).get();
-    }
+  }
+  Future getUserData(String email) async{
+    QuerySnapshot snapshot = await userCollection.where('email', isEqualTo: email).get();
+    print(snapshot.docs[0].data());
+    return snapshot;
+  }
+  getUserGroups() async{
+    return FirebaseFirestore.instance.collection('users').doc(uid).snapshots();
+  }
+  sendMsg(String groupId, chatMsgData){
+    FirebaseFirestore.instance.collection('groups').doc(groupId).collection('messages').add(chatMsgData);
+    FirebaseFirestore.instance.collection('groups').doc(groupId).update({
+      'recentMessage': chatMsgData['message'],
+      'recentMessageSender': chatMsgData['sender'],
+      'recentMessageTime': chatMsgData['time'].toString(),
+    });
+  }
+  getChats(String groupId) async{
+    return FirebaseFirestore.instance.collection('groups').doc(groupId)
+        .collection('messages').orderBy('time').snapshots();
+  }
+  searchByName(String groupName){
+    return FirebaseFirestore.instance.collection('groups').
+    where('groupName', isEqualTo: groupName).get();
   }
 }
